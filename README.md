1a6ecf856061d8758fed06019f8c27fb4a891383
SlowLearning <SlowLearning@users.noreply.github.com>
2020-02-09 21:47:12 -0500

    GM-2/starting from scratch

---
+++ README.md
@@ -0,0 +1,3 @@
+First off, the work by anemolo inspired this website. Absolutely fantastic. 
+
+Three.js is the framework to work on this
---
+++ main.js
@@ -0,0 +1,16 @@
+import * as THREE from 'js/three.module.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = newThree.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement);
